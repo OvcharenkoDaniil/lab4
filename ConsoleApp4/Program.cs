8c1a268f724014b6f08301caac3f1c9ac1f228df
Ovcharenko Daniil <danila-bras@mail.com>
2020-10-26 00:00:21 +0300

    Добавлен объект Owner

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -12,4 +12,28 @@ namespace ConsoleApp4
         {
         }
     }
+    public class List
+    {
+        public class Owner
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string organisation { get; set; }
+
+            public Owner(int id, string name, string organisation)
+            {
+                this.id = id;
+                this.name = name;
+                this.organisation = organisation;
+            }
+        }
+        public Owner owner;
+        public List<int> MyList { get; set;}
+        public List() 
+        {
+            this.MyList = new List<int>() { 1, 2, 3 };
+            this.owner = new Owner(111,"Dima","BSTU");
+        }
+
+    } 
 }
